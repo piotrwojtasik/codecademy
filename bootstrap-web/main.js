a9a39d6d51d7ba751d6b196a56be36553cad2599
Piotr <76587990+piotrwojtasik@users.noreply.github.com>
2022-03-01 14:46:22 +0100

    Delete bootstrap-web directory

--- bootstrap-web/main.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2cGV0IiwiYSI6ImNreDY2bTVvcjFkdXcybm8xNmMzZmQyZGwifQ.JbCBqW9lxKiZ3TXd02hp8g';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 5 // starting zoom
-});
